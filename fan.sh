7cb60823b56bb0f59ab4bee8d6f4b71cd6f56bee
Anatolii Franchuk <lwpssfltr@gmail.com>
2023-04-28 17:10:09 +0300

    Added switcher script, updated fan script to current version

--- fan.sh
+++ fan.sh
@@ -21,9 +21,11 @@ echo "done"
 sp=0
 while true
 do
-	sleep 3
-	temp1=$(cat /sys/class/hwmon/hwmon1/temp4_input)
-	temp2=$(cat /sys/class/hwmon/hwmon1/temp3_input)
+	sleep 2
+
+	temp1=$(cat /sys/class/hwmon/hwmon3/temp2_input)
+	temp2=$(cat /sys/class/hwmon/hwmon3/temp3_input)
+
 	if [ "$temp1" -gt "$temp2" ]
 	then
 		temp="$temp1"
@@ -31,42 +33,58 @@ do
 		temp="$temp2"
 	fi
 
-	if [ "$temp" -lt 44000 ]
-	then
-		if [ "$sp" -ne 1 ]
-		then
-			echo "0" > "$ctrlpath$dfast"
-			echo "0" > "$ctrlpath$dmed"
-			echo "0" > "$ctrlpath$dslow"
-			sp=1
-		fi
-	elif [ "$temp" -ge 44000 -a "$temp" -lt 54000 ]
-	then
-		if [ "$sp" -gt 2 ]
-		then
-			echo "0" > "$ctrlpath$dfast"
-			echo "0" > "$ctrlpath$dmed"
-			echo "1" > "$ctrlpath$dslow"
-			sp=2
-		fi
-	elif [ "$temp" -ge 54000 -a "$temp" -lt 64000 ]
-	then
-		if [ "$sp" -ne 3 ]
-		then
-			echo "0" > "$ctrlpath$dfast"
-			echo "1" > "$ctrlpath$dmed"
-			echo "0" > "$ctrlpath$dslow"
-			sp=3
-		fi
-	elif [ "$temp" -ge 64000 ]
-	then
-		if [ "$sp" -ne 4 ]
-		then
-			echo "1" > "$ctrlpath$dfast"
-			echo "0" > "$ctrlpath$dmed"
-			echo "0" > "$ctrlpath$dslow"
-			sp=4
-		fi
-	fi
+	case "$sp" in
+		0)
+			if [ "$temp" -gt 58000 ]
+			then
+				echo "0" > "$ctrlpath$dfast"
+				echo "0" > "$ctrlpath$dmed"
+				echo "1" > "$ctrlpath$dslow"
+				sp=1
+			fi
+			;;
+
+		1)
+			if [ "$temp" -lt 44000 ]
+			then
+				echo "0" > "$ctrlpath$dfast"
+				echo "0" > "$ctrlpath$dmed"
+				echo "0" > "$ctrlpath$dslow"
+				sp=0
+			elif [ "$temp" -gt 62000 ]
+			then
+				echo "0" > "$ctrlpath$dfast"
+				echo "1" > "$ctrlpath$dmed"
+				echo "0" > "$ctrlpath$dslow"
+				sp=2
+			fi
+			;;
+
+		2)
+			if [ "$temp" -lt 52000 ]
+			then
+				echo "0" > "$ctrlpath$dfast"
+				echo "0" > "$ctrlpath$dmed"
+				echo "1" > "$ctrlpath$dslow"
+				sp=1
+			elif [ "$temp" -gt 65000 ]
+			then
+				echo "1" > "$ctrlpath$dfast"
+				echo "0" > "$ctrlpath$dmed"
+				echo "0" > "$ctrlpath$dslow"
+				sp=3
+			fi
+			;;
+
+		3)
+			if [ "$temp" -lt 62000 ]
+			then
+				echo "0" > "$ctrlpath$dfast"
+				echo "1" > "$ctrlpath$dmed"
+				echo "0" > "$ctrlpath$dslow"
+				sp=2
+			fi
+			;;
+		esac
 	echo "$temp1 $temp2 $temp $sp"
 done
